8d399d89d3e30db4ff3bb5bc9218f616761920ec
Suhani Sethi <suhanisethi22.2007@gmail.com>
2025-11-25 11:25:09 +0530

    Update 1.c

--- EXPERIMENT1/1.c
+++ EXPERIMENT1/1.c
@@ -1,9 +1,10 @@
-
-#include <stdio.h>
+// This code is part of a C program that prints "hello world" to the console.
+#include <stdio.h> // Include standard input-output header
 
 int main(void)
 {
 	printf("hello world\n");
+	printf("hello world\n");// Print "hello world"
 	return 0;
 }
     
